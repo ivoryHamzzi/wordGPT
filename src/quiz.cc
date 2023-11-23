9e4fa9fefdabf4096a0f8e5ad5a7a63f35fa0c4e
Minseon Choi <dollypoll2@naver.com>
2023-11-23 20:34:02 +0900

    Quiz class edited (FIX quiz.cc)

--- src/quiz.cc
+++ src/quiz.cc
@@ -1,13 +1,48 @@
 #include "quiz.h"
 
 template<class T>
-Prob Quiz<T>::question(Dict<T>& dict, string uname = "admin")
+Prob Quiz<T>::question(Dict<T>& dict, bool if_from, string uname = "admin")
 {
     return Prob();
 }
 
-template<class T>
-Prob QuizW2W::question(Dict<T>& dict, string uname = "admin")
+Prob QuizE2K::question(Dict<EngDef>& dict, string uname = "admin")
 {
-    
+        //Get from openai the question (English)
+        //Give an response with 4 multiple choices (Korean)
+        //then get the answer.
+}
+
+Prob QuizK2E::question(Dict<EngDef>& dict, string uname = "admin")
+{
+        //Get from openai the question (Korean)
+        //Give an response with 4 multiple choices (English)
+        //then get the answer.
+}
+
+Prob QuizC2K::question(Dict<ChnDef>& dict, string uname = "admin")
+{
+        //Get from openai the question (Chinese)
+        //Give an response with 4 multiple choices (Korean)
+        //then get the answer.
+}
+
+Prob QuizK2C::question(Dict<ChnDef>& dict, string uname = "admin")
+{
+        //Get from openai the question (Korean)
+        //Give an response with 4 multiple choices (Chinese)
+        //then get the answer.
+}
+
+Prob QuizJ2K::question(Dict<JpnDef>& dict, string uname = "admin")
+{
+        //Get from openai the question (Japanese)
+        //Give an response with 4 multiple choices (Korean)
+        //then get the answer.
+}
+Prob QuizK2J::question(Dict<JpnDef>& dict, string uname = "admin")
+{
+        //Get from openai the question (Korean)
+        //Give an response with 4 multiple choices (Japanese)
+        //then get the answer.
 }
