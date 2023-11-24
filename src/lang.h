c57a7420bcbe14295055c7f37754a26a8b801df8
song2J <ejs1214@naver.com>
2023-11-24 21:00:15 +0900

    question draft implemented, lang.h ';' deleted

--- src/lang.h
+++ src/lang.h
@@ -8,9 +8,9 @@ using namespace std;
 
 class LangDef {
 public:
-    LangDef(const string& w, const string& d):word(w),def(d){};
-    LangDef(const string& w):word(w),def(""){};
-    LangDef():word(""),def(""){};
+    LangDef(const string& w, const string& d):word(w),def(d){}
+    LangDef(const string& w):word(w),def(""){}
+    LangDef():word(""),def(""){}
 
     const string& getWord();
     void setWord(const string& w);
@@ -29,7 +29,7 @@ protected:
 
 class EngDef: public LangDef{
 public:
-    EngDef(const string& w, const string& d, const string& p) : pron(p), LangDef(w, d) {};
+    EngDef(const string& w, const string& d, const string& p) : pron(p), LangDef(w, d) {}
 
     const string& getPronunc();
     void setPronunc(const string& p);
@@ -45,7 +45,7 @@ private:
 
 class JpnDef: public LangDef {
 public:
-    JpnDef(const string& w, const string& d, const string& p) : pron(p), LangDef(w, d) {};
+    JpnDef(const string& w, const string& d, const string& p) : pron(p), LangDef(w, d) {}
 
     const string& getPronunc();
     void setPronunc(const string& p);
@@ -66,7 +66,7 @@ private:
 class ChnDef: public LangDef {
 public:
     ChnDef(const string& w, const string& d, const string& k, string& en)
-        : kanxi(k), eng_rep(en), LangDef(w, d) {};
+        : kanxi(k), eng_rep(en), LangDef(w, d) {}
 
     void setKanxi(string k);
     const string& getKanxi();
@@ -89,7 +89,7 @@ private:
 class KorDef: public LangDef{
 public:
     KorDef(const string& w, const string& d, const string& p, string& h)
-        : pron(p), hanja_rep(h), LangDef(w, d) {};    
+        : pron(p), hanja_rep(h), LangDef(w, d) {}  
 
     const string& getPronunc();
     void setPronunc(const string& p);
--- src/quiz.cc
+++ src/quiz.cc
@@ -4,11 +4,10 @@
 
 using namespace std;
 
-void Quiz<EngDef>::question(Dict<EngDef>& dict, bool if_from, User& user)
+void Quiz<EngDef>::question(Dict<EngDef>& dict, bool if_from)
 {
         //Get from openai the question (English)
         //Give an response from 4 multiple choices (Korean)
-
         string gpt_in = "Give me a question with a single English word in\
         4 choices(1, 2, 3 and 4.) of Korean. The probability for\
         all of the choices to be an answer must be same.\
@@ -16,16 +15,33 @@ void Quiz<EngDef>::question(Dict<EngDef>& dict, bool if_from, User& user)
         Set difficulty of question for TOEIC 900.";
 
         //then get the answer.
+        string prob;
+        string ans;
+        bool isAns = true;
+
+        Prob p;
+        p.prob=prob;
+        p.ans=ans;
+        p.if_right=isAns;
+        p.word_id=dict.retId();
+
+        records.push_back(p);
+
+        //load details from gpt anyway
+        KorDef korean(prob, string(), string(), string());
+        EngDef eng(ans, "", "");
+        dict.addMap(p.word_id, korean, eng);
+        
 }
 
-void Quiz<ChnDef>::question(Dict<ChnDef>& dict, bool if_from, User& user)
+void Quiz<ChnDef>::question(Dict<ChnDef>& dict, bool if_from)
 {
         //Get from openai the question (Chinese)
         //Give an response with 4 multiple choices (Korean)
         //then get the answer.
 }
 
-void Quiz<JpnDef>::question(Dict<JpnDef>& dict, bool if_from, User& user)
+void Quiz<JpnDef>::question(Dict<JpnDef>& dict, bool if_from)
 {
         //Get from openai the question (Japanese)
         //Give an response with 4 multiple choices (Korean)
